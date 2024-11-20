805d2f825d6847dffd3870dcf48c54e595218bca
possessedAlkadien <katesl.pchi@gmail.com>
2024-11-20 22:39:40 +0300

    Add files via upload
    чтение

--- OOp2.py
+++ OOp2.py
@@ -1,21 +1,44 @@
 import os
 
+data = []
+
 class File:
     def filePath(self):
+        print("Для выхода введите 0.")
         path = str(input("Введите путь к файлу:"))
         return path
-    def readFile(self, path):
+
+    def findFile(self, path):
+        if path == '0':
+            return path
         if os.path.exists(path):
-            file = open(path)
-            for row in file:
-                print(row)
+            return path
         else:
-            print("Произошла ошибка в чтении файла.")
+            print("Произошла ошибка! Такого файла не существует.")
+
+    def xmlFile(self,file):
+        print("q")
+
+    def csvFile(self,file):
+        print("q")
 
 
 def main():
-    userFile = File()
-    userFile.readFile(userFile.filePath())
+    ExitFlag = False
+    while (ExitFlag != True):
+        userFile = File()
+        path = userFile.findFile(userFile.filePath())
+        if path == '0':
+            ExitFlag = True
+            print("Закрытие программы...")
+        else:
+            file = open(path)
+            if '.xml' in path:
+                userFile.xmlFile(file)
+            elif '.csv' in path:
+                userFile.csvFile(file)
+            else:
+                print("Формат файла не поддерживается.")
 
 if __name__ == '__main__':
     main()
